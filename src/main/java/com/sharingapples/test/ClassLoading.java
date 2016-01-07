8e157d3e996c239be5d355088e3a4023e0440acd
rpidanny <abhishekmaharjan1993@gmail.com>
2016-01-07 15:27:33 +0545

    Class Loader working

---
+++ src/main/java/com/sharingapples/test/ClassLoading.java
@@ -0,0 +1,22 @@
+package com.sharingapples.test;
+
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+/**
+ * Created by rpidanny on 1/7/16.
+ */
+public class ClassLoading {
+
+  static public void main(String ... Args){
+    InputStream f = ClassLoading.class.getResourceAsStream("demo.txt");
+    // For files outside package
+    // getClass().getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF");
+    try {
+      System.out.println((char)f.read());
+    }catch (Exception e){
+      System.out.println(e);
+    }
+
+  }
+}
