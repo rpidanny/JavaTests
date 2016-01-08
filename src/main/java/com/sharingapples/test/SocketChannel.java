d77d184a56b4c7c85ced22c7160af1532b60a26a
rpidanny2 <abhishek@sharingapples.com>
2016-01-08 15:12:31 +0545

    regular io socket created

---
+++ src/main/java/com/sharingapples/test/SocketChannel.java
@@ -0,0 +1,30 @@
+package com.sharingapples.test;
+
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+
+/**
+ * Created by rpidanny on 1/8/16.
+ */
+public class SocketChannel {
+
+  public static void main(String ... Args){
+    try {
+      Selector selector = Selector.open();
+      ServerSocketChannel ssc = ServerSocketChannel.open();
+      ssc.configureBlocking(false);
+
+      ServerSocket ss = ssc.socket();
+      InetSocketAddress address = new InetSocketAddress(8080);
+      ss.bind(address);
+
+      SelectionKey key = ssc.register(selector, SelectionKey.OP_ACCEPT);
+
+    }catch (Exception e){
+      System.out.println(e);
+    }
+  }
+}
